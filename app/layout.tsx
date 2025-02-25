cf38fd4603bb6683bbfdd4ed81e6180208e2de1d
jonchius <jonchius@gmail.com>
2025-02-25 14:53:20 -0500

    adding styles file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import styles from '../styles/globals.css'
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={styles}>{children}</body>
     </html>
   )
 }
