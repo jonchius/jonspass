6da7f825fdbb9f10a66c86d67140874a1c9b6d45
jonchius <jonchius@gmail.com>
2025-02-25 16:29:49 -0500

    committing minimal product

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Link from "next/link"
-
 export default function Main() {
+
+  const generatePassword = (passwordSize) => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+    let password = ""
+    for (let i = 0; i < passwordSize; i++) {
+      const index = Math.floor(Math.random() * characters.length)
+      password += characters[index]
+    }    
+    return password
+  }
+
   return (
     <>
-      <h1>Home</h1>
-      <p><Link href="/about">About</Link></p>
+      <h1>JONSPASS</h1>      
+
+      <p>Here is your random password: </p>
+
+      <p className="password">{generatePassword(12)}</p>
     </>
   )
 }
